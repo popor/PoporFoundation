3bb920355a1d47a7bcde392fb8b564c518689b65
wangkq <908891024@qq.com>
2018-06-29 16:14:48 +0800

    0.0.6

---
+++ Example/PoporFoundationTV/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PoporFoundationTV
+//
+//  Created by apple on 2018/6/29.
+//  Copyright © 2018年 popor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Example/PoporFoundationTV/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PoporFoundationTV
+//
+//  Created by apple on 2018/6/29.
+//  Copyright © 2018年 popor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
