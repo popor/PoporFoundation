96888e5438015a4d0f3cac33058b0f1802e0a94f
wangkq <908891024@qq.com>
2019-07-15 14:01:53 +0800

    NSUrl 部分插件无法忽略,tvos(9.0, 9.0),无法屏蔽

--- PoporFoundation/Classes/NSObject/NSObject+Swizzling.h
+++ PoporFoundation/Classes/NSObject/NSObject+Swizzling.h
@@ -11,7 +11,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-typedef id (^OCDynamicHookUtilsImpCallback)(id self,...);
+typedef id _Nullable (^OCDynamicHookUtilsImpCallback)(id self,...);
 
 @interface NSObject (Swizzling)
 
